0ae7d152618acb919c6780bdc47a063102cc85ee
KyleTryon <git@techsquidtv.com>
2022-11-15 08:38:53 -0500

    first commit

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+echo 'www.hailsquid.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+
+git push -f git@github.com:KyleTryon/HailSquid-Website.git main:gh-pages
+
+cd -
